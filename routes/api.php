fcc1c34b7251110a35624b26d91d157850927aa8
UrayMR <umrajsyafiq@gmail.com>
2025-07-11 16:07:22 +0700

    refactor: api routes with resource on user

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return response()->json([
-        'status' => true,
-        'message' => 'User data retrieved.',
-        'data' => $request->user(),
-    ]);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class)->names('users');
 });
